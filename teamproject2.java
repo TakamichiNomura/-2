a88041594b79d2bcc741b93b6e8e7116a6551d7f
TakamichiNomura <kennn3065@gmail.com>
2019-10-07 13:11:34 +0900

    class안에

--- teamproject2.java
+++ teamproject2.java
@@ -1,11 +1,24 @@
-
+import java.io.*;
+import java.util.*;
 /**
  * Write a description of class teamproject2 here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (2018243144노무라타카미치)
+ * @version (20191007)
  */
 public class teamproject2
 {
-    
+    public static void main(String[] args){
+        var hm = new HashMap<String,Integer>();
+        try{
+            FileReader fr = new FileReader("d:\\inputData20191007.txt");
+            int c;
+            while((c = fr.read()) != -1){
+                System.out.print((char)c);
+            }
+            fr.close();
+        }catch(IOException e){
+            System.out.println("파일이 없습니다.");
+        }
+    }
 }
