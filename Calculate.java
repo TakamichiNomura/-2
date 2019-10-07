026a73c19a283d84e8a55326fd21ee1202913a07
BangDaeHo <hanava1@naber.com>
2019-10-07 13:08:58 +0900

    calculate 클래스 생성 해쉬맵 이름은 a로 임시 설정

---
+++ Calculate.java
@@ -0,0 +1,23 @@
+import java.util.*;
+/**
+ * Write a description of class Colculate here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Calculate
+{
+    public static void main(String [] args){
+        Set<String> Keys = a.KeySet();
+        Iterator<String> it = Keys.iterator();
+        
+        while(it.hasNext()){
+            String name = it.next();
+            int total =+ a.get(name);            
+        }
+        System.out.println("------ 계산 결과 ------");
+        System.out.println("총점 : " + total);
+        System.out.println("평균 : " + total / a.size());
+        
+    }
+}
